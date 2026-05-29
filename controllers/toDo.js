f0026bcebeaf8dd475eecaadda485cb896532c6f
pratiksanap01 <sanappratik07@gmail.com>
2026-05-29 12:11:59 +0530

    added update todo controller

--- controllers/toDo.js
+++ controllers/toDo.js
@@ -65,3 +65,36 @@ export const deleteTodo = async (req, res) => {
         });
     }
 };
+export const updateTodo = async (req, res) => {
+
+    try {
+
+        const id = req.params.id;
+
+        const updatedTodo = await Todo.findOneAndUpdate(
+            {
+                _id: id,
+                user: req.user.id
+            },
+            req.body,
+            {
+                new: true
+            }
+        );
+
+        if(!updatedTodo) {
+            return res.status(404).json({
+                message: "Todo not found"
+            });
+        }
+
+        return res.json({
+            message: "Todo updated successfully",
+            updatedTodo
+        });
+    } catch(error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+};
--- routes/toDo.js
+++ routes/toDo.js
@@ -5,7 +5,8 @@ import authMiddleware from "../middlewares/auth.js";
 import {
     createTodo,
     getTodos,
-    deleteTodo
+    deleteTodo,
+    updateTodo
 } from "../controllers/toDo.js";
 
 const router = express.Router();
@@ -13,5 +14,6 @@ const router = express.Router();
 router.post("/", authMiddleware, createTodo);
 router.get("/", authMiddleware, getTodos);
 router.delete("/:id", authMiddleware, deleteTodo);
+router.put("/update-todo/:id", authMiddleware, updateTodo);
 
 export default router;
