cd06bb22418968dbb9dcab4f08316765e46228f5
dnnetroc <dhanshree@net-roc.com>
2026-08-07 23:05:27 +0530

    adding friend system

---
+++ backend/models/friendsRequest.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const friendsRequestSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true },
+);
---
+++ oop.cpp
@@ -0,0 +1,17 @@
+ /*multilevel inheritance 
+class is created and defined and class
+class A{ };
+classB: public A{};
+class C:publicB{};
+
+In the class take protected and private data members.
+
+
+ */
+#include<iostream>
+class person{
+protected :
+public: 
+    string name;
+    int age;
+}
