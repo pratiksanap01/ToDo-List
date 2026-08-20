bb120c2f5967aa916447d22a738d882062d3d9a8
dnnetroc <sanappratik07@gmail.com>
2026-08-20 23:10:55 +0530

    remove friend

--- backend/controllers/sendFriendRequest.js
+++ backend/controllers/sendFriendRequest.js
@@ -200,3 +200,25 @@ export const rejectFriendRequest = async (req, res) => {
         });
     }
 };
+
+export const removeFriend = async (req, res) => {
+    try {
+    const userID = req.user.id;
+    const removeFriendID = req.params.removeFriendID;
+
+
+    const user = await User.findById(userID);
+
+    if(!user){
+        return res.status(404).json({
+            message: "User not found!!"
+        })
+
+        
+    }
+    } catch(error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
